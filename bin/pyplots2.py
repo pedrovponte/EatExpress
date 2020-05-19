2e8c74034c956b5d47cf5a33f229335aa5ef6ab5
Eduardo Brito <developer.edurbrito@gmail.com>
2020-05-19 13:32:20 +0100

    Started Phase 3 on Report

--- bin/pyplots2.py
+++ bin/pyplots2.py
@@ -11,9 +11,7 @@ print(floyd)
 plt.title('Dijkstra vs Floyd Warshall | Grid 16 x 16')
 
 plt.plot(list(dijkstra['n']), list(dijkstra['us']),'r-', 
-list(floyd['n']), list(floyd['us']), 'g-', 
-list(dijkstra['n']), [i / 2 for i in dijkstra['us']], 'b-',
-list(dijkstra['n']), [i * r.randint(8,12) / 13 for i in dijkstra['us']], 'y-')
+list(floyd['n']), list(floyd['us']), 'g-')
 
 plt.axis([0, 2200, 0,  dijkstra['us'][7] + 1000])
 plt.ylabel("Tempo Total (us)")
@@ -21,4 +19,4 @@ plt.xlabel("Número de Pedidos")
 plt.xticks(range(0,2200,250))
 plt.legend("DF")
 
-plt.savefig("../logs/phase2/Dijkstra.png")
+plt.savefig("../logs/phase2/Dijkstr-Floyd.png")
